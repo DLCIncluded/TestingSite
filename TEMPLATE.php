ebd549d0bb94381ce8be7b402fbaed74ff761add
DLCIncluded <carydavid85@gmail.com>
2018-02-02 23:04:35 -0500

    Modifications to template file

--- TEMPLATE.php
+++ TEMPLATE.php
@@ -1,3 +1,19 @@
-<?PHP include_once("includes/top.php"); ?>
-Page data goes here
-<?PHP include_once("includes/bottom.php"); ?>
+<?PHP 
+include_once("includes/top.php"); 
+
+	$page = basename(__FILE__, '.php'); 
+	
+	$sql = "SELECT * FROM Pages WHERE name='".$page."'";
+	
+	$result = $connection->query($sql); 
+	
+	if($result->num_rows > 0){
+		while($row = $result->fetch_assoc()){
+			echo $row['pageData'];
+		}
+	} else {
+		echo "No page found.";
+	}
+	
+include_once("includes/bottom.php"); 
+?>
