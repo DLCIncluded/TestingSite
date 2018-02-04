2da9d4835d65af8f2c71cbf2f4a2087740435ea5
DLCIncluded <carydavid85@gmail.com>
2018-02-04 13:30:15 -0500

    Fixed link to edit profile page

--- includes/miniProfile.php
+++ includes/miniProfile.php
@@ -1,2 +1,2 @@
 Welcome to your mini profile, please choose an option<br>
-<a href="profile.php?username=<?PHP echo $username; ?>">View Profile</a> | <a href="editProfile.php?username=<?PHP echo $username; ?>">Settings</a>  | <?PHP if($siteLevel=='5'){ ?><a href="admin-remake.php">Admin Page</a> | <?PHP } ?> <a href="includes/logout.php">Logout</a>
+<a href="profile.php?username=<?PHP echo $username; ?>">View Profile</a> | <a href="editprofile.php?username=<?PHP echo $username; ?>">Settings</a>  | <?PHP if($siteLevel=='5'){ ?><a href="admin-remake.php">Admin Page</a> | <?PHP } ?> <a href="includes/logout.php">Logout</a>
