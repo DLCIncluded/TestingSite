0ac70ffb72576dca785790b02a6b7a1826f47481
DLCIncluded <carydavid85@gmail.com>
2018-02-11 20:51:40 -0500

    Disabling send message button, not implemented yet

--- chat.php
+++ chat.php
@@ -31,19 +31,9 @@ if(isset($_POST['message'])){
 
 
 ?>
-<!--
-<form action="chat.php" method="POST">
-	<input type="text" name="message" placeholder="type your message"/>
-	<input type="submit" name="msgBtn" value="Send Message" />
-</form>
--->
 
-<form action="../multicraft/index.php?r=server/chat&amp;id=1" method="post">
-	<input type="hidden" value="ac4706609c03b0bf65416bb8f0fc5f930e15329b" name="YII_CSRF_TOKEN" />
-	<div class="input-group">
-		<input type="text" id="message" name="message" value="" class="form-control" data-focus>
-		<span class="input-group-btn">
-			<input class="btn btn-primary" type="submit" name="yt0" value="Send" id="yt0" />            </span>
-	</div>
-	<div class="flash-error" id="chat-error" style="display: none"></div>
+<form action="chat.php" method="POST">
+	<input type="text" name="message" placeholder="NOT IMPLEMENTED YET"/>
+	<input type="submit" name="msgBtn" value="Send Message" disabled />
 </form>
+
